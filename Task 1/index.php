25702c80adf685b463ad292bbc1dc6ef1db456d8
Vladislav.Bykon <Vladislav.Bykon@x5.ru>
2023-03-08 23:11:03 +0500

    change to API and add new README

--- Task 1/index.php
+++ Task 1/index.php
@@ -1,53 +1,69 @@
 <?php
 
-$path = __DIR__ . '/../../data/users.csv';
-
-// Проверяем, существует ли файл и доступен ли он для чтения
-if (!is_readable($path)) {
-    die("File $path does not exist or is not readable");
+// Подключение к базе данных
+function connectToDatabase(): PDO
+{
+    $options = [
+        PDO::ATTR_EMULATE_PREPARES => false,
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    ];
+    $dsn = 'mysql:host=localhost;dbname=database_name;charset=utf8mb4';
+    $pdo = new PDO($dsn, 'username', 'password', $options);
+    return $pdo;
 }
 
-// Устанавливаем параметры соединения с базой данных
-$options = [
-    PDO::ATTR_EMULATE_PREPARES => false,
-    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-];
+// Функция для вставки данных из CSV-файла в базу данных
+/**
+ * @throws Exception
+ */
+function insertUsersFromCsv($filePath)
+{
+    $pdo = connectToDatabase();
+    $query = "INSERT INTO users (name, number) VALUES (?, ?)";
+    $stmt = $pdo->prepare($query);
+    $stmt->bindParam(1, $name);
+    $stmt->bindParam(2, $number);
 
-// Подключаемся к базе данных с помощью объекта PDO
-$dsn = 'mysql:host=localhost;dbname=database_name;charset=utf8mb4';
-$pdo = new PDO($dsn, 'username', 'password', $options);
+    // Чтение данных из CSV-файла и вставка их в базу данных
+    if (($handle = fopen($filePath, 'r')) !== false) {
+        $pdo->beginTransaction();
 
-// Запрос на вставку данных в базу данных
-$query = "INSERT INTO users (name, number) VALUES (?, ?)";
-$stmt = $pdo->prepare($query);
+        while (($data = fgetcsv($handle, 0, ",")) !== false) {
+            $name = $data[1] ?? '';
+            $number = $data[0] ?? '';
 
-// Читаем данные из CSV-файла и вставляем их в базу данных
-if (($handle = fopen($path, 'r')) !== false) {
-    // Начинаем транзакцию для оптимизации вставки данных
-    $pdo->beginTransaction();
+            if (empty($name) || empty($number)) {
+                continue;
+            }
 
-    while (($data = fgetcsv($handle, 1000)) !== false) {
-        $name = $data[1] ?? '';
-        $number = $data[0] ?? '';
-
-        // Пропускаем пустые значения в CSV-файле
-        if (empty($name) || empty($number)) {
-            continue;
+            try {
+                $stmt->execute();
+            } catch (PDOException $e) {
+                $pdo->rollBack();
+                throw new Exception("Error inserting data: " . $e->getMessage());
+            }
         }
 
-        // Добавляем данные в буфер подготовленного выражения
-        $stmt->execute([$name, $number]);
+        $pdo->commit();
+
+        fclose($handle);
+    } else {
+        throw new Exception("Failed to open file $filePath");
     }
-
-    // Завершаем транзакцию, чтобы все изменения были сохранены
-    $pdo->commit();
-
-    fclose($handle);
-
-} else {
-    die("Failed to open file $path");
 }
 
-// Оптимизация: закрываем соединение с базой данных
-$pdo = null;
+// Определение маршрута API для вставки данных из CSV-файла в базу данных
+if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['file_path'])) {
+    $filePath = $_POST['file_path'];
+    try {
+        insertUsersFromCsv($filePath);
+        echo 'Data inserted successfully';
+    } catch (Exception $e) {
+        header('HTTP/1.1 500 Internal Server Error');
+        echo $e->getMessage();
+    }
+} else {
+    header('HTTP/1.1 400 Bad Request');
+    echo 'Invalid request';
+}
